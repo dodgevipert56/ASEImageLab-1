199cd048094cef1fcf1d2f1617a7e18af1e91c10
Daria Kozlova <daria@MacBook-Pro-Daria.local>
2018-10-13 14:59:24 +0300

    Added function for cleaning screan

--- main.cpp
+++ main.cpp
@@ -17,20 +17,23 @@ void setPixel(size_t _x, size_t _y,
     _image[offset + 2] = _b;
 }
 
-void clearScreen (char _r, char _g, char _b,
+void clearScreen (unsigned char _r, unsigned char _g, unsigned char _b,
                   std::unique_ptr<unsigned char []> &_image)
 {
 
+    for (size_t i = 0; i < width*height*pixelDepth; i=i+3)
+    {
+        _image[i] = _r;
+        _image[i+1] = _g;
+        _image[i+2] = _b;
+    }
 }
 
 int main() 
 {
     std::unique_ptr<unsigned char []> image = std::make_unique<unsigned char []>(width * height * pixelDepth);
-    
-    for (size_t i = 0; i < width*height*pixelDepth; ++i)
-    {
-        image[i] = 255;
-    }
+
+    clearScreen(0,0,255,image);
 
     for (size_t x = 0; x < width; ++x)
     {
@@ -39,6 +42,7 @@ int main()
     }
 
     Magick::Image output(width, height, "RGB", Magick::CharPixel,image.get());
+
     output.write("test.bmp");
     
     return EXIT_SUCCESS;
