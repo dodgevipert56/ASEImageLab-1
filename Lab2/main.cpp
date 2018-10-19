5f9acd3fab19bc6195bf7681c5b5dfcb42702681
kozlovadg <kozlova.dg@yandex.ru>
2018-10-19 10:56:49 +0100

    Animated sequence of image folder

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -2,11 +2,13 @@
 #include <cstdlib>
 #include <Magick++.h>
 #include <memory>
+#include<random>
 
 const size_t width = 100;
 const size_t height = 100;
 const size_t pixelDepth = 3;
 
+/*
 void setPixel(size_t _x, size_t _y,
               unsigned char _r, unsigned char _g, unsigned char _b,
               std::unique_ptr<unsigned char []> &_image)
@@ -16,6 +18,7 @@ void setPixel(size_t _x, size_t _y,
     _image[offset + 1] = _g;
     _image[offset + 2] = _b;
 }
+*/
 
 void clearScreen (unsigned char _r, unsigned char _g, unsigned char _b,
                   std::unique_ptr<unsigned char []> &_image)
@@ -31,19 +34,50 @@ void clearScreen (unsigned char _r, unsigned char _g, unsigned char _b,
 
 int main() 
 {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> colour(0,255);
+
     std::unique_ptr<unsigned char []> image = std::make_unique<unsigned char []>(width * height * pixelDepth);
 
+    // Same as function setPixel, but in lambda realisation
+    auto setPixel=[&image](size_t _x, size_t _y,
+            unsigned char _r, unsigned char _g, unsigned char _b)
+    {
+        auto offset = (_y * width) * 3 + (_x * 3);
+        image[offset] = _r;
+        image[offset + 1] = _g;
+        image[offset + 2] = _b;
+    };
+
+    /*
     clearScreen(0,0,255,image);
 
     for (size_t x = 0; x < width; ++x)
     {
-        setPixel(x,x,255,0,0,image);
-        setPixel(width-x,x,255,0,0,image);
+        setPixel(x,x,255,0,0);
+        setPixel(width-x,x,255,0,0);
+    }
+    */
+
+    //srand(time(nullptr));
+
+    char fileName[20];
+
+    for (size_t frame=0; frame<100; frame++){
+
+        for (size_t y=0; y<height; y++)
+            for (size_t x=0; x<width; x++) {
+                //setPixel(x,y,rand()%255, rand()%255, rand()%255);
+                setPixel(x,y, colour(gen), colour(gen), colour(gen));
+            }
+
+        sprintf(fileName, "image.%03d.bmp", frame);
+        Magick::Image output(width, height, "RGB", Magick::CharPixel,image.get());
+        output.depth(10);
+        std::cout<<"Writing file "<<fileName<<"\n";
+        output.write(fileName);
     }
 
-    Magick::Image output(width, height, "RGB", Magick::CharPixel,image.get());
-
-    output.write("test.bmp");
-    
     return EXIT_SUCCESS;
 }
